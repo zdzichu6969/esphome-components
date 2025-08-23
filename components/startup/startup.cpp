5721809607b1fac04596351e2effe08327b42cad
Mateusz Gruszczyński <zdzichu6969@gmail.com>
2025-08-23 16:56:28 +0200

    Update startup.cpp

--- components/startup/startup.cpp
+++ components/startup/startup.cpp
@@ -9,7 +9,9 @@ namespace startup {
 
 static const char *const TAG = "startup";
 
-StartupSensor::StartupSensor() : start_(millis()){};
+StartupSensor::StartupSensor() : start_(millis()) {
+  this->set_object_id((get_mac_address() + "-startup").c_str());
+}
 
 void StartupSensor::dump_config() { LOG_SENSOR("", "Startup Sensor", this); }
 
